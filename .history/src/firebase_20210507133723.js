0ef90871b506f1c8e9ab35dea6717f8cbefa2e3a
Randy Steele <steelerx155@Gmail.com>
2021-05-07 16:07:25 -0400

    added firebase

---
+++ .history/src/firebase_20210507133723.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyCS0uN8upOixSE6qV3OTRQdEZRVB-lkcgM",
+    authDomain: "fb-clone-7a1d2.firebaseapp.com",
+    projectId: "fb-clone-7a1d2",
+    storageBucket: "fb-clone-7a1d2.appspot.com",
+    messagingSenderId: "934744093231",
+    appId: "1:934744093231:web:0726b4417eaaa22cef8cda",
+    measurementId: "G-YW09NS7CTS"
+};
