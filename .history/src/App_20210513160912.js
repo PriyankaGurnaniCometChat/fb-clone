ae678cdddf3c7dace3f8603c86dd6797ba90e189
Randy Steele <steelerx155@Gmail.com>
2021-05-14 17:28:51 -0400

    added widgets

---
+++ .history/src/App_20210513160912.js
@@ -0,0 +1,24 @@
+import './App.css';
+import Header from "./Header";
+import Sidebar from "./Sidebar";
+import SidebarRow from './SidebarRow';
+import Feed from "./Feed";
+
+
+function App() {
+  return (
+    <div className="app">
+
+      <Header />
+      <div className="app__body">
+        <Sidebar />
+        <Feed />
+
+      </div>
+      <Widgets />
+
+    </div>
+  );
+}
+
+export default App;
